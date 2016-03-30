28476ba31f847b77fa5ba05ebfdb6adb6eaa8828
Jonas Holbech <jh@jonasholbech.dk>
2016-03-30 10:16:22 +0200

    Localstorage.js added

---
+++ Localstorage.js
@@ -0,0 +1,52 @@
+"use strict";
+console.log("Database loaded");
+var Database = {
+    storageAvailable:function(type) {
+        try {
+            var storage = window[type],
+                x = '__storage_test__';
+            storage.setItem(x, x);
+            storage.removeItem(x);
+            return true;
+        }
+        catch(e) {
+            return false;
+        }
+    },
+    init:function(){
+        if (this.storageAvailable('localStorage')) {
+            // Yippee! We can use localStorage awesomeness
+        }
+        else {
+            throw new Error('Localstorage doesn\'t work in this browser!');
+        }
+    },
+    get:function(key){
+        if(!key){
+            throw new Error('You must pass key to Get!');
+        }
+        return localStorage.getItem(key);
+    },
+    set:function(key, value){
+        if(!key ){
+            throw new Error('You must pass key/value to set!');
+        }
+        localStorage.setItem(key, value)
+    },
+    delete:function(key){
+        if(!key){
+            throw new Error('You must pass key to delete!');
+        } else {
+            localStorage.removeItem(key)
+        }
+    },
+    escape:function(item){
+        return JSON.stringify(item);
+    },
+    unescape:function(item){
+        return JSON.parse(item);
+    },
+    drop:function(){
+        localStorage.clear();
+    }
+};
