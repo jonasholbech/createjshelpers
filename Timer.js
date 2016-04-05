5ff2d44fb9c915f6532a39fe055780ed6aeeaad1
Jonas Holbech <jofh@kea.dk>
2016-04-05 22:44:40 +0200

    Timer.js added

---
+++ Timer.js
@@ -0,0 +1,26 @@
+"use strict"
+function Timer(callback, delay) {
+    var timerId, start, remaining = delay;
+
+    this.pause = function() {
+        window.clearTimeout(timerId);
+        remaining -= new Date() - start;
+    };
+
+    this.resume = function() {
+        start = new Date();
+        window.clearTimeout(timerId);
+        timerId = window.setTimeout(callback, remaining);
+    };
+
+    this.resume();
+}
+/*
+var timer = new Timer(function() {
+    alert("Done!");
+}, 1000);
+
+timer.pause();
+// Do some stuff...
+timer.resume();
+    */
